943101d804b8f65b99421813c877159e12df45b2
Lenochkaa <flylilianagud@gmail.com>
2022-01-22 13:56:48 +0200

    js4hm

---
+++ main.js
@@ -0,0 +1,161 @@
+// begin 1
+// let glor = prompt("Введите сторону квадрата:")
+// let P = 4*glor
+// alert(P)
+// console.log(P)
+
+//BEGIN 25
+// let x = prompt("Введите значение x:")
+// let y = 3*(x**6)-6*(x**2)-7
+// alert(y)
+// console.log(y)
+
+//begin 2
+// let a = prompt("Введите сторону квадрата:")
+// let S = a**2
+// alert(S)
+// console.log(S)
+
+//BEGIN 3
+// let a = prompt("Введите первую сторону прямоугольника")
+// let b = prompt("Введите вторую сторону прямоугольника")
+// let S = a*b
+// let P = (2*a)+(2*b)
+// alert("P = " + P + "; " + "S = " + S +"; ")
+// console.log(P)
+// console.log(S)
+
+// BEGIN 4
+// let d = prompt("Введите диаметр окружности")
+// let L = 3.14*d
+// alert(L)
+// console.log(L)
+
+//begin 5
+// let a = prompt("Введите длину ребра куда")
+// let S = 6*(a**2)
+// let V = a**3
+// console.log(S)
+// console.log(V)
+// alert("S = " + S +"; " + "V = " + V + ";")
+
+//begin 6
+// let a = prompt("Введите длину ребра A")
+// let b = prompt("Введите длину ребра B")
+// let c = prompt("Введите длину ребра B")
+// let V = a*b*c
+// let S = 2*(a*b+b*c+a*c)
+// alert("S = " + S +"; " + "V = " + V + ";")
+// console.log(S)
+// console.log(V)
+
+//begin 7
+// let R = prompt("Введите радиус")
+// let L = 2*3.14*R
+// let S = 3.14*(R**2)
+// alert("S = " + S +"; " + "L = " + L + ";")
+// console.log(L)
+// console.log(S)
+
+//BEGIN 8
+// let a = Number(prompt("Введите первое число"))
+// let b = Number(prompt("Введите второе число"))
+// let Sr = (a + b)/2
+// alert(Sr)
+// console.log(Sr)
+
+//BEGIN 9
+// let a = prompt("Введите первое число")
+// let b = prompt("Введите второе число")
+// let SrGeo = (a*b)**(0.5)
+// alert(SrGeo)
+// console.log(SrGeo)
+
+//begin 10
+// let a = Number(prompt("Введите первое число"))
+// let b = Number(prompt("Введите второе число"))
+// let a2 = a**2
+// let b2 = b**2
+// let Sum = a2+b2
+// let ASum = a2-b2
+// let Mn = a2*b2
+// let AMn = a2/b2
+// alert("Сумма = " + Sum + "; " + "Разница = " + ASum + "; " + "Произведение = " + Mn + "; " + "Частное = " + AMn + ".")
+// console.log(Sum)
+// console.log(ASum)
+// console.log(Mn)
+// console.log(AMn)
+
+//begin 11
+// let a = Number(prompt("Введите первое число"))
+// let b = Number(prompt("Введите второе число"))
+// let a2 = Math.abs(a)
+// let b2 = Math.abs(b)
+// let Sum = a2+b2
+// let ASum = a2-b2
+// let Mn = a2*b2
+// let AMn = a2/b2
+// alert("Сумма = " + Sum + "; " + "Разница = " + ASum + "; " + "Произведение = " + Mn + "; " + "Частное = " + AMn + ".")
+// console.log(Sum)
+// console.log(ASum)
+// console.log(Mn)
+// console.log(AMn)
+
+//begin 12
+// let a = Number(prompt("Введите первый катет"))
+// let b = Number(prompt("Введите второй катет"))
+// let c = ((a**2)+(b**2))**(0.5)
+// let P = a+b+c
+// alert("Гипотенуза = " + c + "; " + "P = " + P)
+// console.log(c)
+// console.log(P)
+
+//BEGIN 13
+// let R1 = Number(prompt("Введите первый радиус"))
+// let R2 = Number(prompt("Введите второй радиус"))
+// let S1 = 3.14*(R1**2)
+// let S2 = 3.14*(R2**2)
+// let S3 = S1-S2
+// alert("S1 = " + S1 + "; " + "S2 = " + S2 + "; " + "S3 = " + S3 + ".")
+// console.log(S1)
+// console.log(S2)
+// console.log(S3)
+
+//begin 14
+// let L = Number(prompt("Введите длину окружности"))
+// let R = L/(2*3.14)
+// let S = 3.14*(R**2)
+// alert("R = " + R + "; " + "S = " + S + ".")
+// console.log(R)
+// console.log(S)
+
+//begin 15
+// let S = Number(prompt("Введите площадь окружности"))
+// let D2 = (S/3.14)*4
+// let D = D2**(0.5)
+// let L = 3.14 * D
+// alert("D = " + D + "; " + "L = " + L + ".")
+// console.log(D)
+// console.log(L)
+
+//BEGIN 26
+// let x = Number(prompt("y=4*(x-3)^6-7*(x-3)^3+2. Введите х "))
+// let y = 4*((x-3)**6)-7*((x-3)**3)+2
+// console.log(y)
+// alert(y)
+
+//begin 27
+// let A = prompt("Введите число А")
+// let A2 = A**2
+// let A4 = A**4
+// let A8 = A**8
+// alert("A2 = " + A2 + "; " + "A4 = " + A4 + "; " + "A8 = " + A8 + ".")
+
+//BEGIN 28
+let A = prompt("Введите число А")
+let A2 = A**2
+let A3 = A**3
+let A5 = A**5
+let A10 = A**10
+let A15 = A**15
+alert("A2 = " + A2 + "; " + "A3 = " + A3 + "; " + "A5 = " + A5 + "; " + "A10 = " + A10 + "; " + "A15 = " + A15 + ".")
